cba12d933e39a10ff39df860d9e14d46232fedc5
abechihiro <abe.1.0.2.3.mssp@gmail.com>
2021-09-21 11:16:27 +0900

    ストア少々、Detailも追加

--- src/store/index.js
+++ src/store/index.js
@@ -4,11 +4,21 @@ import firebase from 'firebase'
 
 Vue.use(Vuex)
 
+const initialState = {
+  Pizzas: [],
+  Toppings: [{id:1, name:'トマト', priceM:200, priceL:300}],
+}
+
 export default new Vuex.Store({
-  state: {
-  },
+  state: initialState,
+
   mutations: {
+    fetchItems(state, Item){            
+      state.Pizzas = Item.Pizza || state.Pizzas
+      state.Toppings = Item.Topping || state.Toppings      
+    },
   },
+
   actions: {
     login(){
     const google_auth_provider = new firebase.auth.GoogleAuthProvider()
@@ -17,7 +27,22 @@ export default new Vuex.Store({
     logout() {
       firebase.auth().signOut()
     },
+
+    fetchItems({ commit }){      
+      firebase.firestore().collection(`Items`).get().then( querySnapshot => {                
+        querySnapshot.forEach( doc => {             
+          commit('fetchItems', doc.data())              
+        })        
+      })
+    },
   },
-  modules: {
+
+  getters: {
+    getPizzas: state => state.Pizzas,
+    getToppings: state => state.Toppings,
+
+    
+    getPizzasById: state => id => state.Pizzas.filter( i => id === i.id)[0] ,
+    getToppingsById: state => id => state.Toppings.filter( i => id === i.id)[0] ,
   }
 })
